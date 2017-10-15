d8e0958536f363421016d87e75f8d791fe6004f6
Rayen Kamta <rayen.kamta@uniphyhealth.com>
2017-10-15 02:06:52 -0400

    login

--- AnimeFreakz/ViewController.swift
+++ AnimeFreakz/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
+
 
