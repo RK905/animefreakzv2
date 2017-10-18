43868a42f09274f72601db3952681eac5c678bb5
Rayen Kamta <rayen.kamta@uniphyhealth.com>
2017-10-17 22:44:09 -0400

    icons & dash

---
+++ AnimeFreakz/DashboardVC.swift
@@ -0,0 +1,47 @@
+//
+//  DashboardVC.swift
+//  AnimeFreakz
+//
+//  Created by Rayen Kamta on 10/17/17.
+//  Copyright © 2017 Rayen Kamta. All rights reserved.
+//
+
+import UIKit
+
+class DashboardVC: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var nav = self.navigationController?.navigationBar
+        nav?.barStyle = UIBarStyle.black
+        nav?.tintColor = UIColor.white
+        nav?.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.orange]
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AnimeFreakz/ViewController.swift
+++ AnimeFreakz/ViewController.swift
@@ -7,27 +7,188 @@
 //
 
 import UIKit
-import QuartzCore
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var usernameView: UIView!
-    
+    @IBOutlet weak var passView: UIView!
+    @IBOutlet weak var loginView: UIView!
+  
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-     
+     self.passView.layer.borderWidth = 1
+     self.passView.layer.borderColor = UIColor.loginColor().cgColor
+     self.loginView.layer.borderWidth = 1
+     self.loginView.layer.borderColor = UIColor.loginColor().cgColor
+        
+        
+        let defaults = UserDefaults.standard
+        if let name = defaults.string(forKey: "userNameKey") {
+            let pass = defaults.string(forKey: "passWordKey")
+            Auth.auth().signIn(withEmail: name, password: pass!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
 
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBOutlet weak var saveSwitch: UISwitch!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func saveSwitch(_ sender: Any) {
+        
+    }
+    @IBAction func pressSave(_ sender: Any) {
+        if(saveSwitch.isOn){
+            saveSwitch.setOn(false, animated: true)
+        }else {
+            saveSwitch.setOn(true, animated: true)
+        }
+    }
     
+    @IBAction func pressLogin(_ sender: Any) {
+        if(self.saveSwitch.isOn){
+            let defaults = UserDefaults.standard
+            defaults.set(self.usernameField.text, forKey: "userNameKey")
+            defaults.set(self.passwordField.text, forKey: "passWordKey")
+            defaults.synchronize()
+        }else{
+            let defaults = UserDefaults.standard
+            defaults.set("", forKey: "userNameKey")
+            defaults.set("", forKey: "passWordKey")
+            defaults.synchronize()
+        }
+        
+        if self.usernameField.text == "" || self.passwordField.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            Auth.auth().signIn(withEmail: self.usernameField.text!, password: self.passwordField.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 
-
+    @IBAction func pressRegister(_ sender: Any) {
+        if usernameField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: usernameField.text!, password: passwordField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+//                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+//                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func pressForgotPassword(_ sender: Any) {
+        if self.usernameField.text! == "" {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter an email in the Username Field", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().sendPasswordReset(withEmail: self.usernameField.text!, completion: { (error) in
+                
+                var title = ""
+                var message = ""
+                
+                if error != nil {
+                    title = "Error!"
+                    message = (error?.localizedDescription)!
+                } else {
+                    title = "Success!"
+                    message = "Password reset email sent."
+                   
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            })
+        }
+    }
 }
 
 
